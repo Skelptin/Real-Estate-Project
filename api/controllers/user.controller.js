65b20e6e89ebb044d728735b3c4aa569b96ce25c
Mayank Painkra <mpainkra7@gmail.com>
2024-03-21 01:13:46 +0530

    created listing api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import bcrypt from 'bcrypt'
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({
@@ -46,7 +47,26 @@ export const deleteUser = async (req, res, next) => {
     res.clearCookie('access_token');
     res.status(200).json('User has been deleted!')
   } catch (err) {
-    next(err)
+    next(err);
+  }
+}
+
+export const getUserListings = async (req, res, next) => {
+
+  if (req.user.id === req.params.id) {
+
+    try {
+
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings)
+
+    } catch (err) {
+      next(err);
+    }
+
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'))
   }
 
-}
+
+} 
