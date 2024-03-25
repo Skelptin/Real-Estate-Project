2211de125c32cbf0886024d7d2dc6b16229349f9
Mayank Painkra <mpainkra7@gmail.com>
2024-03-26 02:02:25 +0530

    update header search form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -8,11 +8,31 @@ import './Header.css'
 
 const Header = () => {
 
+  const navigate = useNavigate();
+
+  const [searchTerm, setSearchTerm] = useState('')
+
 
   const { currentUser } = useSelector((state) => state.user)
 
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl)
+    }
+  }, [])
 
   return (
     <header className="bg-blue-200 nav shadow-md"  >
@@ -24,14 +44,18 @@ const Header = () => {
           </h1>
         </Link>
 
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             className="bg-transparent focus:outline-none w-24 sm:w-64"
             type="text"
             placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
 
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
 
         <ul className="flex gap-4">
